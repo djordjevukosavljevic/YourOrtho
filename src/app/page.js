a22a9112d85eb14caa071f9ddc2fdd1081f820bd
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-06-18 23:41:14 +0200

    commented unneeded for now

--- src/app/page.js
+++ src/app/page.js
@@ -13,12 +13,6 @@ export default function Home() {
           priority
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Ispitaj sta hoce covek
-          </li>
-          <li className="mb-2 tracking-[-.01em]">
-            Pozadinu, boju, single page, contact formu itd
-          </li>
           {/* <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
@@ -31,7 +25,10 @@ export default function Home() {
           </li> */}
         </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+
+
+        {/* Default buttons */}
+        {/* <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -55,10 +52,12 @@ export default function Home() {
           >
             Read our docs
           </a>
-        </div>
+        </div> */}
       </main>
+
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
+        {/* Default footer */}
+        {/* <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
@@ -102,7 +101,7 @@ export default function Home() {
             height={16}
           />
           Go to nextjs.org →
-        </a>
+        </a> */}
       </footer>
     </div>
   );
