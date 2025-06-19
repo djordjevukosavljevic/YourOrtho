9b245a3e0c101edfc26ebd72e90db7822ea93eca
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-06-19 11:13:41 +0200

    ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { format } from "path";
+
 export default function HomePage(){
     return (
         <main>
+            <h1 className="h3 text-center">Welcome</h1>
         </main>
     );
 }
