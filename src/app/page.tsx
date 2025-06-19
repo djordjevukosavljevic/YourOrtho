a23df9ca8b245fd5af930dcbde8a202625e2827e
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-06-19 11:35:46 +0200

    trying to add locmotive scroll library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+'use client';
 import { format } from "path";
+import { useEffect } from "react";
+import '../styles/page.module.css'
 
-export default function HomePage(){
+export default function HomePage() {
+
+    useEffect(() => {
+        {
+            async () => {
+                const LocomotiveScroll = (await import('locomotive-scroll')).default;
+                const locomotiveScroll = new LocomotiveScroll();
+            }
+        }
+    }, [])
+
     return (
         <main>
             <h1 className="h3 text-center">Welcome</h1>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
         </main>
     );
 }
