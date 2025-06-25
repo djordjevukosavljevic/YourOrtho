a361415cf7f6ddc1d2391fdf97082708616fc7fd
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-06-25 11:58:29 +0200

    changing icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import React  from "react";
 export const metadata = {
   title: 'Radiology',
   description: 'Diagnostic imaging services with modern equipment',
+  icons:{
+    icon: '/favicon.ico'
+  }
 }
 
 export default function RootLayout({
