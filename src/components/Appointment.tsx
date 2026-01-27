d0758b03740e86c855b51de7bfb71e5d7356b2c2
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2026-01-27 16:04:53 +0100

    adding appointment frontend still

--- src/components/Appointment.tsx
+++ src/components/Appointment.tsx
@@ -2,10 +2,11 @@
 
 import contactMsg from "../js/contact.js";
 
-export default function Contacts() {
+export default function Appointment() {
     return (
         <div>
-            <h2 className="text-3xl font-bold mb-6 text-center">Contact Us</h2 >
+            <h2 className="text-3xl font-bold mb-6 text-center">Book an appointment</h2>
+            <small><h4 className="text-3x1 mb-6 text-center">but first choose date</h4></small>
             <form className="w-full max-w-md space-y-4">
                 <input
                     type="text"
@@ -45,7 +46,7 @@ export default function Contacts() {
                     type="button"
                     className="w-full p-3 bg-blue-600 text-white rounded hover:bg-blue-700"
                 >
-                    Send Message
+                Schedule
                 </button>
             </form>
             <p className="mt-6 text-gray-600 text-center">📍 Belgrade | ☎️ +38162231689 | ✉️ <a href="mailto:djordje.vukosavljevic01@gmail.com">vvukosavljevic@gmail.com</a></p>
